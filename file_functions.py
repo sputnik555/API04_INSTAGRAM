1a976c26cf711f7b678ca1d456c05693bd8fab26
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2021-07-28 23:51:33 +0300

    Update file_functions.py
    change creating directories

--- file_functions.py
+++ file_functions.py
@@ -12,19 +12,14 @@ IMAGE_SIZE = 1080
 def download_file(url, file_name):
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    download_path = Path(os.getenv("DOWNLOAD_FOLDER"))
-    if not download_path.exists():
-        download_path.mkdir()
+    download_path = make_dir(os.getenv("DOWNLOAD_FOLDER"))
 
     with open(download_path / file_name, 'wb') as file:
         file.write(response.content)
 
 
 def convert_image(file_name):
-    converted_path = Path(os.getenv("CONVERTED_FOLDER"))
-    if not converted_path.exists():
-        converted_path.mkdir()
-
+    converted_path = make_dir(os.getenv("CONVERTED_FOLDER"))
     image = Image.open(Path(os.getenv("DOWNLOAD_FOLDER")) / file_name)
     image.thumbnail((IMAGE_SIZE, IMAGE_SIZE))
     image.save(converted_path / (os.path.splitext(file_name)[-2] + '.jpg'), format='JPEG')
@@ -37,3 +32,9 @@ def get_url_tail(url):
 
 def get_file_extension(url):
     return os.path.splitext(urllib.parse.urlparse(url).path)[-1]
+
+
+def make_dir(dir_name):
+    new_path = Path(dir_name)
+    new_path.mkdir(exist_ok=True)
+    return new_path
