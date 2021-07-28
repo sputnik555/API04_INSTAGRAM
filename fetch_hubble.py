9f27cb6b10817f4625e89b9be70851bd377e8a11
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2021-07-28 19:07:33 +0300

    Add file_functions.py
    new module for common functions

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -2,27 +2,7 @@ import requests
 import os
 from urllib.parse import urlsplit
 from dotenv import load_dotenv
-from PIL import Image
-
-
-def download_file(url, file_name):
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-
-    if not os.path.exists(os.getenv("DOWNLOAD_FOLDER")):
-        os.makedirs(os.getenv("DOWNLOAD_FOLDER"))
-
-    with open(os.getenv("DOWNLOAD_FOLDER") + '\\' + file_name, 'wb') as file:
-        file.write(response.content)
-
-
-def convert_image(file_name):
-    if not os.path.exists(os.getenv("CONVERTED_FOLDER")):
-        os.makedirs(os.getenv("CONVERTED_FOLDER"))
-
-    image = Image.open(os.getenv("DOWNLOAD_FOLDER") + '\\' + file_name)
-    image.thumbnail((1080, 1080))
-    image.save(os.getenv("CONVERTED_FOLDER") + '\\' + os.path.splitext(file_name)[-2] + '.jpg', format='JPEG')
+import file_functions
 
 
 def download_image_by_id(id):
@@ -34,8 +14,8 @@ def download_image_by_id(id):
     file_url = 'https:' + image.get('file_url')
     file_name = str(id) + get_file_extension(file_url)
     try:
-        download_file(file_url, file_name)
-        convert_image(file_name)
+        file_functions.download_file(file_url, file_name)
+        file_functions.convert_image(file_name)
     except Exception:
         pass
 
