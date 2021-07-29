733a2d65e59a73021d294ade4619f02f5069760a
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2021-07-30 00:16:25 +0300

    Update fetch_spacex.py
    dry code

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 def fetch_spacex_last_launch(download_folder):
     response = requests.get('https://api.spacexdata.com/v4/launches/latest')
     response.raise_for_status()
-    image_links = response.json().get('links').get('flickr').get('original')
+    image_links = response.json()['links']['flickr']['original']
 
     for image in image_links:
         file_name = file_functions.get_url_tail(image)
@@ -18,4 +18,7 @@ if __name__ == '__main__':
     load_dotenv()
     converted_folder = os.getenv('CONVERTED_FOLDER')
     download_folder = os.getenv('DOWNLOAD_FOLDER')
-    fetch_spacex_last_launch(download_folder)
+    try:
+        fetch_spacex_last_launch(download_folder)
+    except Exception:
+        print('Ошибка загрузки файлов')
